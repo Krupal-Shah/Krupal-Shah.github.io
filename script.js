314cb58a5313eaba27c8469f646791d671131312
Krupal Shah <krupalshah74@gmail.com>
2023-10-06 01:22:55 -0600

    Update backend

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 'use strict';
 
+const nav = document.getElementById('navi-btn');
+
+// Function to check if the end of a section is at the top of the viewport
+function isElementAtTop(element) {
+  const rect = element.getBoundingClientRect();
+  console.log(rect)
+  return rect.bottom <= -100;
+}
+
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
+    const header = document.getElementById('head');
   
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
@@ -14,6 +24,12 @@ function reveal() {
         reveals[i].classList.remove("active");
       }
     }
+
+    if (isElementAtTop(header)) {
+        nav.classList.remove('hide');
+    } else {
+        nav.classList.add('hide');
+    }
   }
 
 window.addEventListener("scroll", reveal);
@@ -24,3 +40,5 @@ $('.slider').flickity({
   wrapAround: true,
   freeScroll: true
 });
+
+
